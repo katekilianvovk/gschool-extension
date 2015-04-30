602660bec6f86e4eb3245a8735401a349a13f6ca
Annie Lydens <aelydens@gmail.com>
2015-04-29 23:15:58 -0600

    cleaning things up

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var year = 31558464000,
-    day = 86400000,
-    hour = 3600000,
-
-    hoursLeftToday = document.getElementById("hoursLeft"),
+var hoursLeftToday = document.getElementById("hoursLeft"),
     daysLeftMonth = document.getElementById("daysLeftThisMonth"),
-    gSchoolLeft = document.getElementById("gSchoolDaysLeft");
+    gSchoolLeft = document.getElementById("gSchoolDaysLeft"),
+    DAY_IN_MS = 86400000,
+    HOUR_IN_MS = 3600000;
 
 setInterval(function updateTab(){
   var newDate = new Date();
@@ -15,15 +13,15 @@ setInterval(function updateTab(){
 
 function hoursLeft(newDate) {
   var timeLeftToday = (new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate(), 24, 0)).getTime() - newDate.getTime();
-  return timeLeftString = (timeLeftToday / hour).toString().substring(0, 7);
+  return (timeLeftToday/HOUR_IN_MS).toString().substring(0, 7);
 }
 
 function daysLeft(newDate) {
   var timeLeftThisMonth = (new Date(newDate.getFullYear(), newDate.getMonth() + 1, 1)) - newDate.getTime();
-  return (timeLeftThisMonth / day).toString().substring(0, 8);
+  return (timeLeftThisMonth/DAY_IN_MS).toString().substring(0, 8);
 }
 
 function gSchoolDaysLeft(newDate) {
   var timeLeft = (new Date(2015, 6, 3).getTime()) - newDate.getTime();
-  return (timeLeft / day).toString().substring(0, 8);
+  return (timeLeft/DAY_IN_MS).toString().substring(0, 8);
 }
